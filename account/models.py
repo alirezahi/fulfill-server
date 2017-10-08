e45800a1d863963ba1dca233978e82d24c21061d
Alireza <alireza97hi@gmail.com>
2017-10-08 10:46:02 +0330

    server initialized

---
+++ account/models.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class FulfillUser(models.Model):
+    """
+    fulfill users model that have specific user information
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    happiness = models.IntegerField(default=0)
+    health = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+    coins = models.IntegerField(default=0)
+    hearts = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+    score = models.IntegerField(default=0)
+
+
+class Task(models.Model):
+    """
+    task models that defines each task information and is related to only one user
+    """
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    difficulty = models.CharField(max_length=20)
+    category = models.ForeignKey(Category)
+    due_date = models.DateTimeField()
+    done_progress = models.IntegerField()
+    score = models.IntegerField()
+    user = models.ForeignKey(FulfillUser)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+# t = Task(title='a', description='1', difficulty='1', category='b', due_date='2009-12-12', done_progress=12, score=30, user=f)
---
+++ account/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from account.models import *
+from rest_framework.response import Response
+from django.db.models import Sum
+
+# Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        email = request.POST.get('email','')
+        if User.objects.filter(username=username).count()<1:
+            u = User(username=username,email=email)
+            u.set_password(password)
+            u.save()
+            f = FulfillUser(user=u)
+            f.save()
+            return Response({'status':'okay'},status=200)
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return Response({'status':'ok'},status=200)
+        return Response({'status':'user not exist'},status=404)
+
+
+def make_task(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        title = request.POST.get('title','')
+        description = request.POST.get('description','')
+        difficulty = request.POST.get('difficulty', '')
+        category = request.POST.get('category', '')
+        due_date = request.POST.get('due_date', '')
+        done_progress = request.POST.get('done_progress', '')
+        score = request.POST.get('score', '')
+        f = FulfillUser.objects.get(user=User.objects.get(username=username))
+        t = Task(title=title, description=description, difficulty=difficulty, category=category,
+                 due_date=due_date, done_progress=done_progress, score=score, user=f)
+        t.save()
+
+def categories_rank(request):
+    t = Task.objects.values('category').annotate(Sum('score'))[0]
+    return Response(t)
+
+
+def user_score(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        u = User.objects.get(username=username)
+        f = FulfillUser.get(user=u)
+        result = Task.objects.filter(user=f).aggregate(Sum('score'))
+        return Response(result)
+
+
+def user_rank(request):
+    if request.method == 'GET':
+        username = request.POST.get('username','')
+        rankings = Task.objects.filter(category='a').values('category').annotate(Sum('score'))
+        return Response(rankings)
+
+
+def first_scores(request):
+    if request.method == 'GET':
+        result = {}
+        for i in FulfillUser.objects.all():
+            result[i.user.username] = 0
+            for j in Task.objects.all():
+                if j.user == i:
+                    result[i.user.username] += j.score
+        print(result)
